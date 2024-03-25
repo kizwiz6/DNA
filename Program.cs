dfd883f7f56d3440bad1f9e462be66a25a3d687e
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-25 19:02:33 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DNA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String dna1 = "ATGCGATACGCTTGA";
+            String dna2 = "ATGCGATACGTGA";
+            String dna3 = "ATTAATATGTACTGA";
+
+            String dna = dna1;
+            int length = dna.Length;
+            Console.WriteLine("Length: " + length);
+
+            // Find start codon:
+            int start = dna.IndexOf("ATG");
+            Console.WriteLine("Start: " + start);
+
+            // Find stop codon:
+            int stop = dna.IndexOf("TGA");
+            Console.WriteLine("Stop: " + stop);
+
+            if (start != -1 && stop != -1 && (start - stop) % 3 == 0)
+            {
+                String protein = dna.Substring(start, stop + 3);
+                Console.WriteLine("Protein: " + protein);
+                Console.WriteLine("Condition 1 and 2 are satisfied.");
+            }
+            else
+            {
+                Console.WriteLine("no protein");
+            }
+        }
+    }
+}
